de7ad207b06788926c8dda2b8572566998e3df6b
JacquilineFeliciano <98136048+JacquilineFeliciano@users.noreply.github.com>
2022-05-14 12:01:19 +0800

    Add files via upload

---
+++ Exercise 15/Exercise15.js
@@ -0,0 +1,8 @@
+document.getElementById("FirstName").value = "Jacquiline Feliciano";
+document.getElementById("Age").value = "20";
+document.getElementById("Phone Number").value = "09987456234";  
+document.getElementById("Email Address").value = "20215154@s.ubaguio.edu";  
+document.getElementById("Program").value = "BSCS";  
+document.getElementById("IDNumber").value = "20215154";  
+document.getElementById("Year Level").value = "1st Year";  
+document.getElementById("Section").value = "IAA1";  
